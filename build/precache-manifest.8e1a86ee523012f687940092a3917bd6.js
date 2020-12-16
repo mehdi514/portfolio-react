012dcf4265cab723035e974372a31f6d78d6cac0
mehdi514 <muhammad@devduo.com>
2020-12-16 22:45:04 +0500

    gitignore

---
+++ build/precache-manifest.8e1a86ee523012f687940092a3917bd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd27bb26730942f4975",
+    "url": "/static/css/main.c197fc4c.chunk.css"
+  },
+  {
+    "revision": "6fd27bb26730942f4975",
+    "url": "/static/js/main.6fd27bb2.chunk.js"
+  },
+  {
+    "revision": "a497a014ce93f4792b75",
+    "url": "/static/css/1.30869c88.chunk.css"
+  },
+  {
+    "revision": "a497a014ce93f4792b75",
+    "url": "/static/js/1.a497a014.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ebcefefcbb8702ea789a6509f7ddb2f",
+    "url": "/static/media/zopa.4ebcefef.png"
+  },
+  {
+    "revision": "12501e57f93132e5916074aed86b7d29",
+    "url": "/static/media/LogoWhite@2x.12501e57.png"
+  },
+  {
+    "revision": "4dfe2ee098640c9fdd46b3d4f5edf923",
+    "url": "/static/media/img.4dfe2ee0.png"
+  },
+  {
+    "revision": "96707b760b941739356fe906759aa2da",
+    "url": "/index.html"
+  }
+];
